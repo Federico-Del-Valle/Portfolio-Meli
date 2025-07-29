42649aa38a2f41e06d946042821962ac8e2eff9f
Federico <fedexdelvalle@gmail.com>
2025-07-29 00:38:42 -0300

    Cambios realizados

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Navbar } from './components/navbar/navbar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+  imports: [RouterOutlet, Navbar],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
 })
 export class App {
   protected readonly title = signal('Fede-meli-portfolio');
+  public instrumentos: number[] = [1,2,3,4];
 }
---
+++ src/app/components/navbar/navbar.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  selector: 'app-navbar',
+  imports: [],
+  templateUrl: './navbar.html',
+  styleUrl: './navbar.css'
+})
+
+
+
+export class Navbar {
+
+}
