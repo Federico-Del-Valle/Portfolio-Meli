d4c249e563b6d19afe4a23edb81f4ebfea1cb53a
Federico <fedexdelvalle@gmail.com>
2025-08-14 00:11:35 -0300

    Cambios portfolioPage

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,17 +2,22 @@ import { Component, OnInit } from '@angular/core';
 import { Proyecto } from '../../../models/proyect.interface';
 import { ProyectsService } from '../../../services/proyects.service';
 import { RouterLink } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+type SortKey = 'alfabetico' | 'terminadosPrimero' | 'enProcesoPrimero';
 
 @Component({
   selector: 'app-home',
-  imports: [RouterLink],
+  standalone:true,
+  imports: [RouterLink,FormsModule, CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent implements OnInit{
   categorias: { nombre: string, cantidad: number }[] = [];
   soloTerminados = false;
-
+  selectedSort: SortKey = 'alfabetico';
 
   public proyectos: Proyecto[] = [];
 
@@ -49,9 +54,44 @@ export class HomeComponent implements OnInit{
 
   get proyectosFiltrados(): Proyecto[] {
     return this.soloTerminados
-      ? this.proyectos.filter(p => p.llega) // solo terminados
-      : this.proyectos;                     // todos
+      ? this.proyectos.filter(p => p.llega)
+      : this.proyectos;
   }
+
+
+  // 2) orden final mostrado en la vista
+  get proyectosOrdenados(): Proyecto[] {
+    const data = this.proyectosFiltrados.slice();
+
+    const precio = (p: Proyecto) => p.precio ?? 0;
+    const llegaNum = (p: Proyecto) => (p.llega ? 1 : 0);
+
+    switch (this.selectedSort) {
+      case 'terminadosPrimero':
+        // Terminado (true) primero, y dentro de cada grupo, por precio
+        return data.sort((a, b) =>
+          (llegaNum(b) - llegaNum(a)) ||
+          (precio(b) - precio(a))
+        );
+      case 'enProcesoPrimero':
+        // En proceso (false) primero, y dentro de cada grupo, por precio
+        return data.sort((a, b) =>
+          (llegaNum(a) - llegaNum(b)) ||
+          (precio(b) - precio(a))
+        );
+      case 'alfabetico':
+        // Ordena por el nombre del proyecto de forma alfabética (A-Z)
+        return data.sort((a, b) => a.titulo!.localeCompare(b.titulo!));
+      default:
+        // Más relevantes: ahora se basa solo en el precio y si está terminado
+        return data.sort((a, b) =>
+          (precio(b) - precio(a)) ||
+          (llegaNum(b) - llegaNum(a))
+        );
+    }
+  }
+  trackById = (_: number, p: Proyecto) => p.id;
+
 
 
 
